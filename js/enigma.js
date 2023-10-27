04189bb1c83402d7c2b59f27928b7e49d17a062d
Mhite55 <pierreguyot55@gmail.com>
2023-10-27 12:04:53 +0200

    affiche mait pas calcul

--- js/enigma.js
+++ js/enigma.js
@@ -3,10 +3,9 @@ const ecran = document.getElementById("ecran")
 
 let btn = document.getElementsByClassName("btn")
 
-let precedent = 0
 let affichage = ""
-let operation = null
 let memoire
+let regex = /\d/
 
 console.log(btn)
 for (const element of btn) {
@@ -15,32 +14,32 @@ for (const element of btn) {
 
 function gererTouche(event){
     const listeBtn = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/", ".", "=", "C" ];
-    if(parseFloat(listeBtn) >= 0 || listeBtn === "." ){
+    if(regex.test(this.value)){
         affichage = (affichage === "") ? this.value.toString() : affichage + this.value.toString()
         ecran.innerHTML = affichage
+        console.log("if you know")
     }else{
-        switch (event) {
+        switch (this.value) {
             case "C":
-                console.log("reset enfoire")
+                console.log("reset")
+                affichage = "";
+                ecran.innerHTML = 0
                 break;
             case "+":
             case "-":
             case "*":
             case "/":
+                affichage = (affichage === this.value) ? this.value.toString() : affichage + this.value.toString()
+                console.log(this.value)
                 break;
             case "=":
+                affichage = (affichage === this.value) ? this.value.toString() : affichage + this.value.toString()
+                math.evaluate['']
+                console.log("egale")
                 break;
             default:
                 console.log(`Sorry, i'm done.`)
                 break;
         }
     }
-}
-function calculer(nb1, nb2, operation){
-    nb1 = parseFloat(nb1);
-    nb2 = parseFloat(nb2);
-    if(operation === "+") return nb1 + nb2;
-    if(operation === "-") return nb1 - nb2;
-    if(operation === "*") return nb1 * nb2;
-    if(operation === "/") return nb1 / nb2;
 }
